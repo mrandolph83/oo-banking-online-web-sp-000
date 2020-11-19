b82a3fb18c72b9579840aa546e6f1f96c4fdf8b7
Matt Smith <mrandolph83@gmail.com>
2020-11-19 00:13:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,9 +19,8 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      @status = "closed"
-      return "Transaction rejected. Please check your account balance."
-
+      self.status = "closed"
+      return Transaction rejected. Please check your account balance.
     end
   end
 
